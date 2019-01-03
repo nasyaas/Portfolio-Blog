314464f8e8d391ee0db60d5df3c73b6746516ffb
Nasya Smith <nasyasmith@yahoo.com>
2019-01-03 13:29:46 -0500

    Collage

--- header.js
+++
@@ -1,7 +0,0 @@
-// $(function() {
-//   $(document).scroll(function() {
-//     var $nav = $("#mainNavbar");
-//     var $h1 = $("h1");
-//     $nav.toggleClass("scrolled", $(this).scrollTop() > $h1.height());
-//   });
-// });
